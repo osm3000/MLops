8e58d6da638b6cf142ee795df65cb37e6dce99e8
osm3000 <omar.samir3000@gmail.com>
2022-09-12 09:55:41 +0200

    Changing a bit the format, to test the black workflow

--- CI_CD/aws_lambda/lambda_function.py
+++ CI_CD/aws_lambda/lambda_function.py
@@ -2,13 +2,16 @@
 Implementing basic CI/CD for a AWS lambda function
 """
 import json
+
+random_dict = {"first": True, "second": False}
+
 def lambda_handler(event, context):
     """
     My first Lambda function within CI/CD
     """
-    
+
     print(context)
-    
+
     return {
         "statusCode": 200,
         "body": json.dumps("Hello from Lambda!"),
